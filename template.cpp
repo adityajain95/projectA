54138d487cee470d95e4c1c7787b6998ca8867b4
adityajain95 <adityajain95@yahoo.in>
2022-10-01 12:41:10 +0530

    Added online cp code template

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f first
+#define s second
+#define CLR(x)  memset(x,0,sizeof(x))
+#define RESET(x,a) memset(x,a,sizeof(x))
+#define pi pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define debug(x) cerr<<">value ("<<#x<<") : "<<x<<endl;
+#define mod 1000000007
+#define INF 100000000000000001LL
+#define eps 0.0000001
+
+const int mxN = 1e5;
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
